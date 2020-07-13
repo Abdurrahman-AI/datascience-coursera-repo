e0d990bc5aa08be40a2eb5f6a133792da68a3318
sdx99 <68219517+sdx99@users.noreply.github.com>
2020-07-13 15:13:49 +0530

    Add files via upload

---
+++ RStudio1.R
@@ -0,0 +1,25 @@
+---
+title: "Hello word.md"
+author:"Studio"
+output:
+  ##this is a markdown file
+  toc:TRUE
+---
+  
+  '''{r setup, include=FALSE}
+  knitr::opts_chunk$set(echo = TRUE)
+  '''
+
+## R Markdown
+
+This is an Rmarkdown document
+Markdown is simple formating
+Syntax for authoring HTML,PDF,
+and MS word documents.
+
+'''{r cars}
+summary(cars)
+'''
+
+
+
